d53093454aa852c74eb7d492faaaf0079d3a928d
Anton Oshchepkov <antonoschepkov27@gmail.com>
2023-08-31 08:06:25 +0700

    [*] Discountable.java - исправил грамматическую ошибку в коментарии

--- src/main/java/ru/aston/oshchepkov_aa/task1/Discountable.java
+++ src/main/java/ru/aston/oshchepkov_aa/task1/Discountable.java
@@ -8,7 +8,7 @@ import java.math.BigDecimal;
 public interface Discountable {
     /**
      * Gets the discount percent. Must be in range of [0,1].
-     * @return discount precent from 0.0 to 1.0
+     * @return discount percent from 0.0 to 1.0
      */
     default BigDecimal getDiscountPercent(){
         return BigDecimal.ONE;
